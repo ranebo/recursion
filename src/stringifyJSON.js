32f3f37b642238cde855fbb1bc7ce51064b2cd60
Rane Gridley <ranegri3@gmail.com>
2016-01-18 18:56:54 -0800

    Second Update to stringifyJSON
    Took out last bit of unnecessary code

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -1,18 +1,13 @@
-// this is what you would do if you liked things to be easy:
-// var stringifyJSON = JSON.stringify;
-
-// but you don't so you're going to write it from scratch:
-
 var stringifyJSON = function(obj) {
 
   if (typeof obj === "function" || obj === undefined || obj === null) {
       return 'null';
   }
 
-  else if (Array.isArray(obj)){
+  else if (Array.isArray(obj)) {
     var arr = [];
 
-    _.each(obj, function(item, i){
+    _.each(obj, function(item) {
       arr.push(stringifyJSON(item));
     });
 
@@ -32,7 +27,7 @@ var stringifyJSON = function(obj) {
   }
 
   else {
-    return typeof obj === "string" ? '"' + obj + '"' : _.identity(obj) + ""
+    return typeof obj === "string" ? '"' + obj + '"' : obj + "";
   }
 
 };
